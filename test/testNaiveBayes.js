1e8a8395303d1be1189c48e568dae129d5baab27
Tom Shen <tom@shen.io>
2013-12-22 00:02:19 -0500

    Refactor Naive Bayes test

--- test/testNaiveBayes.js
+++ test/testNaiveBayes.js
@@ -1,41 +1,45 @@
-var ml = require('ml-kit')
+/*
+This test is designed to demonstrate that the Naive Bayes classifier produces
+sane error rates. To run the test, save the 20 Newsgroup data from
+[http://qwone.com/~jason/20Newsgroups/20news-bydate-matlab.tgz] to
+test/data, and run the file.
+*/
+
+var ml = require('../lib/ml-kit')
   , csv = ml.process.csv
   , _ = require('lodash')
-  , fs = require('fs');
-
-// download 20 Newsgroups data from http://qwone.com/~jason/20Newsgroups/20news-bydate-matlab.tgz
+  , fs = require('fs')
+  , RSVP = require('rsvp');
 
 function process_data(name) {
-  csv().from.path('./' + name + '.label').to.array(function (labels) {
-    csv().from.path('./' + name + '.data', { delimiter: ' ' }).to.array(function (data) {
-      var output = {};
-      _.forEach(_.range(1,20+1), function (_class) {
-        output[_class] = {};
-      });
-      _.forEach(data, function (docWordCount, index) {
-        var doc = _.parseInt(docWordCount[0], 10);
-        var word = _.parseInt(docWordCount[1], 10);
-        var count = _.parseInt(docWordCount[2], 10);
-        var _class = _.parseInt(labels[String(doc-1)][0], 10);
-        if (!(doc in output[_class]))
-          output[_class][doc] = {};
-        output[_class][doc][word] = count;
-      });
-      output = _.mapValues(output, function (docs) {
-        return _.values(docs);
-      });
-      fs.writeFile('./' + name + '.json', JSON.stringify(output, null, '\t'), function (err) {
-        if (err) console.error(err);
-        else console.log(name + ' data saved');
+  var promise = new RSVP.Promise(function (resolve, reject) {
+    csv().from.path('./data/' + name + '.label').to.array(function (labels) {
+      csv().from.path('./data/' + name + '.data', { delimiter: ' ' }).to.array(function (data) {
+        var output = {};
+        _.range(1,20+1).forEach(function (_class) {
+          output[_class] = {};
+        });
+        data.forEach(function (docWordCount, index) {
+          var doc = parseInt(docWordCount[0], 10);
+          var word = parseInt(docWordCount[1], 10);
+          var count = parseInt(docWordCount[2], 10);
+          var _class = parseInt(labels[String(doc-1)][0], 10);
+          if (!(doc in output[_class]))
+            output[_class][doc] = {};
+          output[_class][doc][word] = count;
+        });
+        output = _.mapValues(output, function (docs) {
+          return _.values(docs);
+        });
+        resolve(output);
       });
     });
   });
+  return promise;
 }
 
-function testNaiveBayes() {
-  var nb = new ml.classify.NaiveBayes(_.map(_.range(1,20+1), String), _.map(_.range(1,61188+1), String));
-  var trainingData = require('./train.json');
-  var testingData = require('./test.json');
+function testNaiveBayes(trainingData, testingData) {
+  var nb = new ml.classify.NaiveBayes(_.range(1,20+1).map(String), _.range(1,61188+1).map(String));
   var startTrain = _.now();
   nb.train(trainingData);
   console.log('Training took ' + String((_.now() - startTrain) / 1000) + ' seconds.');
@@ -43,7 +47,6 @@ function testNaiveBayes() {
   var totalErrorRate = 0;
   _.forEach(testingData, function (data, _class) {
     nb.classifyInstances(data, function (classifications) {
-      //fs.writeFileSync('tmp-' + _class + '.label', classifications.join('\n'));
       var errorRate = _.filter(classifications, function (c) {
         return c != _class;
       }).length / classifications.length;
@@ -55,7 +58,6 @@ function testNaiveBayes() {
   console.log('Mean error rate: ' + totalErrorRate / 20);
 }
 
-
-process_data('train');
-process_data('test');
-testNaiveBayes();
+RSVP.all([process_data('train'), process_data('test')]).then(function (data) {
+  testNaiveBayes(data[0], data[1]);
+});
